02cc80aaa14975a607f916690f30223cab0c6bcd
bajames9 <davisbailey65@gmail.com>
2025-11-11 19:47:48 -0500

    config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+    plugins: [react()],
+    build: {
+        outDir: 'dist',   // ensures matches buildspec.yml
+    },
+    base: './',          // important for S3 hosting
 })
