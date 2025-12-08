80530d5ec43bb05a693c060625445bb3d2b53790
Bajames9 <davisbailey65@gmail.com>
2025-12-08 18:50:26 -0500

    fin version

--- src/services/api.js
+++ src/services/api.js
@@ -6,7 +6,7 @@
 
 
 // sets the base url of the api
-const BASE_URL = "http://localhost:5000"  //"http://54.167.71.175:5000/"; // e.g., "http://localhost:5000"
+const BASE_URL = "http://54.226.232.204:5000"  //"http://54.167.71.175:5000/"; // e.g., "http://localhost:5000"
 
 //Hits the Login endpoint
 export const getLogin = async (username, password) => {
